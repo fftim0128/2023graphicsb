d0284794bf35d2efd319e828bc03a8190f6088c2
fftim0128 <125547146+fftim0128@users.noreply.github.com>
2023-03-09 16:41:48 +0800

    Add files via upload

---
+++ Week04旋轉/main.cpp
@@ -0,0 +1,22 @@
+#include<GL/glut.h>
+float angle = 0;
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT| GL_DEPTH_BUFFER_BIT);
+    glPushMatrix();//今天新加的程式碼
+        glRotatef(angle,0,1,0);//今天新加的程式碼
+    glutSolidTeapot(0.3);
+    glPopMatrix();
+    glutSwapBuffers();//今天新加的程式碼
+    angle++;
+}
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("week03");
+
+	glutDisplayFunc(display);
+	glutIdleFunc(display);
+    glutMainLoop();
+}
